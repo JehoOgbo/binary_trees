b8326de6a5536ad3576f72634a3c564ea63f6d6f
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-09 20:48:10 +0100

    insert left compile again

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,15 +15,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-
-	node = binary_tree_node(parent, value);
 	
 	if (parent->left == NULL)
 	{
+		node = binary_tree_node(parent, value);
 		parent->left = node;
 	}
 	else
 	{
+		node = binary_tree_node(parent, value);
 		save = parent->left;
 		parent->left = node;
 		node->left = save;

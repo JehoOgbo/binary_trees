5b3bbdd12c35a29eb3f0b66ca3480a6323660819
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-09 21:19:01 +0100

    insert left compile change parent of last

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,8 +3,8 @@
 /**
  * binary_tree_insert_left - inserts a node as the left child of another
  *
- * @parent - the parent node
- * @value - the value to be stored in the new node
+ * @parent: the parent node
+ * @value: the value to be stored in the new node
  *
  * Return: a pointer to the created node
  */
@@ -27,6 +27,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		save = parent->left;
 		parent->left = node;
 		node->left = save;
+		node->left->parent = node;
 	}
 	return (node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,8 +3,8 @@
 /**
  * binary_tree_insert_right - inserts a node as the right child of another
  *
- * @parent - the parent node
- * @value - the value to be stored in the new node
+ * @parent: the parent node
+ * @value: the value to be stored in the new node
  *
  * Return: a pointer to the created node
  */
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
 
 	if (parent->right == NULL)
 	{
